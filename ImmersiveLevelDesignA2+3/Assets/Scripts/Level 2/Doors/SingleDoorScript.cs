49cf3460a8172ae5a0cb8e3547b2806360e5a199
ZacharySs <zacsyntageros@gmail.com>
2019-10-11 20:17:18 +1000

    - More geo work on the room in main lab area - Added sliding door for use in Level 2, complete with animation and trigger

---
+++ ImmersiveLevelDesignA2+3/Assets/Scripts/Level 2/Doors/SingleDoorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleDoorScript : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetTrigger("CycleSingleDoor");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetTrigger("CycleSingleDoor");
+        }
+    }
+}
