7a845c0aa075a33a3be7bbdc330e3eef11dcd041
louiscbirch <louis.c.birch@hotmail.com>
2019-10-15 19:55:24 +1000

    - worked on cameras

---
+++ ImmersiveLevelDesignA2+3/Assets/Scripts/AIandSecurity/ControlPanelScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanelScript : MonoBehaviour
+{
+    public bool playerInRange;
+    public bool CameraDisabled;
+    public int CameraDisbaleTime;
+    
+
+    public GameObject securityCamera;
+
+    private SphereCollider securityCameraTrigger;
+    private Light securityCameraLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraDisabled = false;
+        securityCameraTrigger = securityCamera.GetComponent<SphereCollider>();
+        securityCameraLight = securityCamera.GetComponentInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisableCamera();   
+    }
+
+    IEnumerator ReactivateCamera()
+    {
+        yield return new WaitForSeconds(CameraDisbaleTime);
+        EnableCamera();
+    }
+
+    private void EnableCamera()
+    {
+        securityCameraTrigger.enabled = true;
+        securityCameraLight.enabled = true;
+        CameraDisabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+
+    private void DisableCamera()
+    {
+        if(playerInRange == true & Input.GetKeyDown(KeyCode.E) & securityCameraTrigger.enabled == true)
+        {
+            securityCameraTrigger.enabled = false;
+            securityCameraLight.enabled = false;
+            CameraDisabled = true;
+            StartCoroutine(ReactivateCamera());
+        }
+    }
+}
---
+++ ImmersiveLevelDesignA2+3/Assets/Scripts/AIandSecurity/PatrollingAIMovementScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrollingAIMovementScript : MonoBehaviour
+{
+    NavMeshAgent navMesh;
+    Rigidbody rigidBody;
+    
+    public Transform target;
+    public Transform[] wayPoints;
+    public int currentWaypoint;
+    public float speed;
+    public float stopDistance;
+    public float pauseTimer;
+    [SerializeField]
+    private float currentTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.freezeRotation = true;
+        target = wayPoints[currentWaypoint];
+        currentTimer = pauseTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMesh.acceleration = speed;
+        navMesh.stoppingDistance = stopDistance;
+
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        // Movement
+        if (distance > stopDistance && wayPoints.Length > 0)
+        {
+            target = wayPoints[currentWaypoint];
+        }
+
+        else if (distance <= stopDistance && wayPoints.Length > 0)
+        {
+            if (currentTimer > 0)
+            {
+                currentTimer -= 0.1f;
+            }
+
+            if (currentTimer <= 0)
+            {
+                currentWaypoint++;
+                if (currentWaypoint >= wayPoints.Length)
+                {
+                    currentWaypoint = 0;
+                }
+                target = wayPoints[currentWaypoint];
+                currentTimer = pauseTimer;
+            }
+        }
+        navMesh.SetDestination(target.position);
+    }
+}
