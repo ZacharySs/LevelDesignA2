aee8f03bdf576792679fdbd681bfb6d94e733497
ZacharySs <zacsyntageros@gmail.com>
2019-10-16 13:37:18 +1000

    - Added Level 2 Hallway door (+ script & animation)

---
+++ ImmersiveLevelDesignA2+3/Assets/Scripts/Level 2/Doors/HallwayDoorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HallwayDoorScript : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetTrigger("CycleHallwayDoor");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetTrigger("CycleHallwayDoor");
+        }
+    }
+}
