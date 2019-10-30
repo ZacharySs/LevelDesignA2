37e746c10b709b9374906168237e3075cd223cb8
ZacharySs <zacsyntageros@gmail.com>
2019-10-30 11:37:36 +1000

    - Level3 finalised for Louis - Added placeholder Console for doors.

---
+++ ImmersiveLevelDesignA2+3/Assets/Scripts/DoorConsoleScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorConsoleScript : MonoBehaviour
+{
+    GameObject[] doors;
+    GameObject closestDoor = null;
+
+    public GameObject doorToUnlock;
+    private HallwayDoorScript doorScriptToUnlock;
+
+    void Start()
+    {
+        doors = GameObject.FindGameObjectsWithTag("HallwayDoor");
+        foreach (GameObject door in doors)
+        {
+            if (door.GetComponent<HallwayDoorScript>().isConsoleUnlockable)
+            {
+                if (closestDoor)
+                {
+                }
+            }
+        }
+
+        if (doorToUnlock)
+        {
+            doorScriptToUnlock = doorToUnlock.GetComponent<HallwayDoorScript>();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            UnlockDoor();
+        }
+    }
+
+    private void UnlockDoor()
+    {
+        doorScriptToUnlock.UnlockDoor();
+    }
+}
